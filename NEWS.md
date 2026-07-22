2f80c533229cd8757e1aa78cc48728bb91993624
LabAsim <lampgit@pm.me>
2026-07-22 22:35:54 +0300

    FacialBurns dataset, load lavaan ggdag glue

---
+++ NEWS.md
@@ -0,0 +1,6 @@
+
+# Lmisc 0.1.1
+
+* Added function `modify_parameter_estimates` 
+* Added a vignette to present how to use `plot_dag` with `Lavaan` ouput
+
---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Facial Burns Data
+#'
+#'This dataset was taken from Lavaan 0.6.2. The versions >0.7 have removed it.
+#'
+#' @docType data
+#' @format A data frame with 6 columns and 77 rows
+#' @source Only for demonstration purposes
+#' @name FacialBurns
+"FacialBurns"
